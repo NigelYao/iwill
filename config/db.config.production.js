15d7d35c97934c47408bb7f4f3c041e73a6e7376
Nigel Yao <Nigel Yao>
2016-04-24 20:36:24 +0800

    edit database config

--- config/db.config.production.js
+++ config/db.config.production.js
@@ -1,5 +1,5 @@
 module.exports = {
-    host     : '10.10.26.58',
+    host     : '10.10.26.59',
     user     : 'ucTMBnIVwoYf2hep',
     password : 'p9AqDp37oULtSlBuh',
     database : '26Hf0WEIlRUASOxr',
